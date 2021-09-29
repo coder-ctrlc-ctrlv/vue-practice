42205a23331db2b85c455a89052ec3641385e9a6
krasnikov <s.krasnikov@hotmail.com>
2021-09-29 09:45:39 +0300

    first commit

---
+++ src/components/HomeUserMoney.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "HomeUserMoney"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Record.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Record"
+}
+</script>
+
+<style scoped>
+
+</style>
